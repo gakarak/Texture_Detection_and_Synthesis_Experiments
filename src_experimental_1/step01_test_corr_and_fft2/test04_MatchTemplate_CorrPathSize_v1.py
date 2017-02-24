1b8a3918f439ff42320704a64aa299927307930c
Alexander Kalinovsky (deepw) <gakarak@gmail.com>
2017-02-24 19:05:26 +0300

    next step

--- src_experimental_1/step01_test_corr_and_fft2/test04_MatchTemplate_CorrPathSize_v1.py
+++ src_experimental_1/step01_test_corr_and_fft2/test04_MatchTemplate_CorrPathSize_v1.py
@@ -11,8 +11,20 @@ import glob
 import os
 
 from scipy import ndimage
+from skimage.feature import match_template
+
+
 import peakutils
 # from peakdetect import peakdetect
+
+def matchFullyCorr(timg):
+    tmatch0 = match_template(timg, timg, pad_input=True)
+    # tmatch0 = timg.copy()
+    tmatch = tmatch0.copy()
+    # tmatch[tmatch < 0] = 0
+    tmatch = tmatch ** 2
+    tret = np.abs(np.fft.fft2(tmatch))
+    return (tret, tmatch0)
 
 def getImgGrad(timg):
     tdx = ndimage.sobel(timg, 1)
@@ -20,31 +32,16 @@ def getImgGrad(timg):
     tgrad = np.hypot(tdx, tdy)
     return tgrad
 
-def matchRandomTemplate(timg, paramSizeMin=60, paramSizeMax=64):
-    tsiz = timg.shape
-    tmpSiz = np.random.randint(paramSizeMin, paramSizeMax)
-    tr = np.random.randint(0, tsiz[0] - tmpSiz - 1)
-    tc = np.random.randint(0, tsiz[1] - tmpSiz - 1)
-    tmp = timg[tr:tr+tmpSiz, tc:tc+tmpSiz]
-    tmatch = cv2.matchTemplate(timg, tmp, cv2.TM_CCOEFF)
-    tmatch[tmatch<0] = 0
-    tmatch = tmatch ** 2
-    tmatch = tmatch[:tsiz[0] - paramSizeMax, :tsiz[1] - paramSizeMax]
-    # tret = np.abs(np.fft.fftshift(np.fft.fft2(tmatch)))
-    tret = np.abs(np.fft.fft2(tmatch))
-    tmatch = np.roll(tmatch, shift=-tr, axis=0)
-    tmatch = np.roll(tmatch, shift=-tc, axis=1)
-    return (tret, tmatch)
-
 #################################
 if __name__ == '__main__':
-    wdir = '../../data/data02_simpleCorrSynth'
-    # fimg = '/home/ar/dev-git.git/dev.matlab/DEMO_DATAMOLA/shirts.unzip/2d48586954536851717a414835673866.png'
+    wdir = '../../data/data03_for_demo/ex01_simple'
     lstfimg = glob.glob('%s/*.jpg' % wdir)
     numImages = len(lstfimg)
     numSamples  = 12
     sizeMin     = 60
-    sizeMax     = 64
+    sizeMax     = 16
+    dx = 1
+    dy = 1
     for ii, fimg in enumerate(lstfimg):
         timg = cv2.imread(fimg, cv2.IMREAD_GRAYSCALE)
         tsiz = timg.shape
@@ -53,42 +50,13 @@ if __name__ == '__main__':
         #
         timgCrop = timg[int(tsiz[0]*pbnd):-int(tsiz[0]*pbnd), int(tsiz[1]*pbnd):-int(tsiz[1]*pbnd)].copy().astype(np.float32)
         timgCrop /= 255.
-        # timgCrop = getImgGrad(timgCrop)
-        timgFFT = np.fft.fftshift(np.fft.fft2(timgCrop))
-        timgTMP = timgCrop[50:50+tmpSiz, 50:50+tmpSiz].copy()
-        tmatchT = cv2.matchTemplate(timgCrop, timgTMP, cv2.TM_CCOEFF_NORMED)
-        # tmatchT[tmatchT<0] = 0
-        tmatchT = tmatchT**2
-        # val, tmatchT = cv2.threshold(tmatchT, 0.01, 0, cv2.THRESH_TOZERO)
-        #
-        tmFFT = np.abs(np.fft.fftshift(np.fft.fft2(tmatchT)))
-        # sumFX = np.sum(tmFFT, axis=1)
-        # sumFY = np.sum(tmFFT, axis=0)
-        sumFX = None
-        sumFY = None
-        tmatchSum = None
-        for ss in range(numSamples):
-            tret, tmpm = matchRandomTemplate(timgCrop, )
-            # tret = np.log(tret)
-            if sumFX is None:
-                sumFX = np.sum(tret, axis=1)
-                sumFY = np.sum(tret, axis=0)
-                tmatchSum = tmpm
-            else:
-                sumFX += np.sum(tret, axis=1)
-                sumFY += np.sum(tret, axis=0)
-                tmatchSum += tmpm
-        sumFX /= np.sum(sumFX)
-        sumFY /= np.sum(sumFY)
-        tmatchSum /= numSamples
-        tmatchSum[:10,:10]=0
-        tmatchSum[-10:, -10:] = 0
-        #
-        # pkxIdx = scipy.signal.find_peaks_cwt(sumFX, np.arange(1,5))
-        # pkyIdx = scipy.signal.find_peaks_cwt(sumFY, np.arange(1,5))
+        timgCrop = getImgGrad(timgCrop)
+        tcorrFFT, tcorrMap = matchFullyCorr(timgCrop)
+        sumFX = np.sum(tcorrFFT, axis=1)
+        sumFY = np.sum(tcorrFFT, axis=0)
         # (1) search peaks in freq-domain
-        pkxIdx = peakutils.indexes(sumFX, min_dist=3)
-        pkyIdx = peakutils.indexes(sumFY, min_dist=3)
+        pkxIdx = peakutils.indexes(sumFX, min_dist=2)
+        pkyIdx = peakutils.indexes(sumFY, min_dist=2)
         pkxX   = sumFX[pkxIdx]
         pkyY   = sumFY[pkyIdx]
         # (2) select low-frequency peaks and transform into spatial shifts
@@ -125,32 +93,26 @@ if __name__ == '__main__':
         pxy0 = [int(timgCrop.shape[1] * 0.1), int(timgCrop.shape[0] * 0.1)]
         #
         plt.figure()
-        plt.subplot(3, 3, 1)
+        plt.subplot(2, 3, 1)
         plt.imshow(timgCrop, cmap=plt.gray()), plt.title('Original')
-        plt.subplot(3, 3, 2)
-        plt.imshow(np.log(np.abs(timgFFT)), cmap=plt.gray()), plt.title('FFT2+FFTShift')
-        plt.subplot(3, 3, 3)
-        plt.imshow(timgTMP), plt.title('Template')
-        plt.subplot(3, 3, 4)
-        plt.imshow(tmatchT, cmap=plt.gray()), plt.title('Correlation MAP')
-        plt.subplot(3, 3, 5)
-        plt.imshow(np.log(np.abs(tmFFT)), cmap=plt.gray()), plt.title('Corr-FFT')
-        plt.subplot(3, 3, 6)
+        plt.subplot(2, 3, 2)
+        plt.imshow(np.log(tcorrFFT), cmap=plt.gray()), plt.title('CorrFFT')
+        plt.subplot(2, 3, 3)
         plt.hold(True)
         plt.plot(sumFX)
         plt.plot(pkxIdx, pkxX, 'o')
         plt.title('Freqs: lo-bnd ~%s, hi-bnd ~%s' % (freqLoBnd, freqHiBnd))
         plt.grid(True)
         plt.hold(False)
-        plt.subplot(3, 3, 7)
+        plt.subplot(2, 3, 4)
         plt.hold(True)
         plt.plot(sumFY)
         plt.plot(pkyIdx, pkyY, 'o')
         plt.hold(False)
         plt.grid(True)
-        plt.subplot(3, 3, 8)
-        plt.imshow(tmatchSum)
-        plt.subplot(3, 3, 9)
+        plt.subplot(2, 3, 5)
+        plt.imshow(tcorrMap), plt.title('CorrMap')
+        plt.subplot(2, 3, 6)
         if timgCrop.ndim<3:
             tmpImage = cv2.cvtColor(255.*timgCrop, cv2.COLOR_GRAY2BGR).astype(np.uint8)
         else:
@@ -159,5 +121,4 @@ if __name__ == '__main__':
         cv2.rectangle(tmpImage, (pxy0[0], pxy0[1]), (pxy0[0] + int(4*dx), pxy0[1] + int(4*dy)), (255,0,0), 2)
         plt.imshow(tmpImage)
         plt.title('DX=%s, DY=%s, Size=%s' % (dx, dy, tmpImage.shape[:2]))
-        # plt.scatter(pkx)
         plt.show()
