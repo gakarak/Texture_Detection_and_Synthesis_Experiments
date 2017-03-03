e25f9a03b964ef3730f5c587342f25e2197a2069
pisarik <mr_plum@mail.ru>
2017-03-03 18:08:44 +0300

    refactored corr-synthesis

--- src_experimental_1/step06_simpleCorrSynth/run01_Simple_CorrTextSynth_v4.py
+++ src_experimental_1/step06_simpleCorrSynth/run01_Simple_CorrTextSynth_v4.py
@@ -82,71 +82,7 @@ def MinimizePatternByTemplMatchV3(pattern, brdSize=5, isDebug=False, parMethod =
     posLB = (posMaxV[0] - 1*ptrnSize, posMaxV[1])
     return (tretCrop, posRT, posLB)
 
-def getBestTexton(timg, vx, vy, isGood, sizN=1, parMethod = cv2.TM_SQDIFF):
-    bestTexton = None
-    maxCorr = -10005000000
-    posRT = (0, 0)
-    posLB = (0, 0)
-
-    bestBBox = None
-
-    good_idx = np.transpose(np.where(is_good))
-    for i, j in good_idx:
-        if (i + sizN >= isGood.shape[0] or j + sizN >= isGood.shape[1]
-            or not isGood[i+sizN, j+sizN]):
-            continue
-
-        bbox = getTexton(vx, vy, isGood, i, j, sizN)
-        bbox = bbox * 1500 / 480
-
-        bbW = np.abs(bbox[0][0] - bbox[0][1])
-        bbH = np.abs(bbox[1][0] - bbox[1][1])
-        parBorder = int(min(bbH,bbW) * 0.15)
-        # parBorder = 5
-        print(bbox)
-        print ('Border parameter: %s' % parBorder)
-        #
-        imgTexton = cropTexton(timg, bbox, brdPx=parBorder, brdPrcnt=None)
-
-        ptrnSize = parBorder+2
-        brdSizeExt = int(1.7*parBorder)
-        ptrnLef = imgTexton[ptrnSize:-ptrnSize, :ptrnSize]
-        ptrnTop = imgTexton[:ptrnSize, ptrnSize:-ptrnSize]
-        print(imgTexton.shape)
-        print(ptrnLef.shape)
-        ccMapH = cv2.matchTemplate(imgTexton, ptrnLef, method=parMethod)
-        ccMapV = cv2.matchTemplate(imgTexton, ptrnTop, method=parMethod)
-        if parMethod==cv2.TM_SQDIFF or parMethod==cv2.TM_SQDIFF_NORMED:
-            ccMapH = 1. - ccMapH
-            ccMapV = 1. - ccMapV
-        minH = ccMapH.min()
-        minV = ccMapV.min()
-        ccMapHflt = ccMapH.copy()
-        ccMapVflt = ccMapV.copy()
-        ccMapHflt[:, :-brdSizeExt] = minH
-        ccMapVflt[:-brdSizeExt, :] = minV
-        # pMaxH = np.argmax(ccMapHflt)
-        # pMaxV = np.argmax(ccMapVflt)
-        _, maxVH, _, posMaxH = cv2.minMaxLoc(ccMapHflt)
-        _, maxVV, _, posMaxV = cv2.minMaxLoc(ccMapVflt)
-
-        #print(maxVH + maxVV)
-        if (maxCorr < maxVH + maxVV):
-            #print('#############################################################')
-            bestBBox = bbox
-            shiftH_X, shiftH_Y = posMaxH
-            shiftV_X, shiftV_Y = posMaxV
-            posRT = (posMaxH[0], posMaxH[1] - 1*ptrnSize)
-            posLB = (posMaxV[0] - 1*ptrnSize, posMaxV[1])
-            maxCorr = maxVH + maxVV
-            bestTexton = imgTexton[:shiftV_Y, :shiftH_X]
-
-
-    #print(posRT, posLB)
-    #return (bestTexton, posRT, posLB)
-    return bestBBox
-
-def generateTiledTextonV1(texton, dRightY, dBottomX, nr=5, nc=5):
+def generateTiledTextureV1(texton, dRightY, dBottomX, nr=5, nc=5):
     tsiz = texton.shape[:2]
     sizR, sizC = tsiz
     dRR = np.abs(dRightY  * nr)
@@ -170,7 +106,7 @@ def generateTiledTextonV1(texton, dRightY, dBottomX, nr=5, nc=5):
                 retTexture[rr:rr + sizR, cc:cc + sizC, :] = texton.copy()
     return retTexture
 
-def generateTiledTextonV2(textonBRD, posXY_RT, posXY_LB, nr=5, nc=5, isDebug=False):
+def generateTiledTextureV2(textonBRD, posXY_RT, posXY_LB, nr=5, nc=5, isDebug=False):
     tsiz = textonBRD.shape[:2]
     sizR, sizC = tsiz
     drShiftTot = posRT[1] * (nc - 0)
@@ -210,7 +146,7 @@ def generateTiledTextonV2(textonBRD, posXY_RT, posXY_LB, nr=5, nc=5, isDebug=Fal
         retTexture = retTexture[r0:r0 + (drStep*nr), c0:c0+(dcStep*nc), :]
     return retTexture
 
-def ReadGraph(pdir, shirt_num):
+def readGrid(pdir, shirt_num):
     wdir = '%s/%s_result/' % (pdir, shirt_num )
     v_x = np.loadtxt(wdir+'pts_x.csv', delimiter=',') - 100 - 1 #FIXME: shift in 100px is automaticaly added by Texture Extraction Algorithm
     v_y = np.loadtxt(wdir+'pts_y.csv', delimiter=',') - 100 - 1
@@ -218,19 +154,19 @@ def ReadGraph(pdir, shirt_num):
     return [v_x, v_y, is_good]
 
 ##########################################
-def maskErosion(is_good, percent_of_src = 0.4, min_square = 10):
+def maskErosion(mask, percent_of_src = 0.4, min_square = 10):
     """
         percent_of_src - result square is about percent_of_src of source square
     """
-    blob_is_good = is_good.copy()
+    eroded_mask = mask.copy()
     
-    min_square = max(percent_of_src*np.sum(is_good), min_square)
-    while np.sum(blob_is_good) > min_square:
-        blob_is_good = binary_erosion(blob_is_good)
+    min_square = max(percent_of_src*np.sum(mask), min_square)
+    while np.sum(eroded_mask) > min_square:
+        eroded_mask = binary_erosion(eroded_mask)
 
-    return blob_is_good
+    return eroded_mask
 
-def getTexton(vx, vy, isGood, row, col, sizN=1):
+def getTextonBBox(vx, vy, row, col, sizN=1):
     X = []
     Y = []
     X += [vx[(row + 0   , col + 0)]]
@@ -254,30 +190,94 @@ def getTexton(vx, vy, isGood, row, col, sizN=1):
     return bbox
 
 
-def getRandomTexton(vx, vy, isGood, sizN=1):
-    rows, cols = np.where(isGood)
+def getRandomTextonBBox(vx, vy, mask, sizN=1):
+    rows, cols = np.where(mask)
     idxRnd = np.random.randint(len(rows))
 
     print ('pos = (%d, %d)' % (rows[idxRnd], cols[idxRnd]))
 
-    return getTexton(vx, vy, isGood, rows[idxRnd], cols[idxRnd], sizN)
+    return getTextonBBox(vx, vy, rows[idxRnd], cols[idxRnd], sizN)
+
+def getBestTextonBBox(img, vx, vy, mask, sizN=1, parMethod = cv2.TM_SQDIFF):
+    bestTexton = None
+    maxCorr = -10005000000
+    posRT = (0, 0)
+    posLB = (0, 0)
+
+    bestBBox = None
+
+    good_idx = np.transpose(np.where(mask))
+    for i, j in good_idx:
+        if (i + sizN >= mask.shape[0] or j + sizN >= mask.shape[1]
+            or not mask[i+sizN, j+sizN]):
+            continue
+
+        bbox = getTextonBBox(vx, vy, i, j, sizN)
+        bbox = bbox * 1500 / 480
+
+        bbW = np.abs(bbox[0][0] - bbox[0][1])
+        bbH = np.abs(bbox[1][0] - bbox[1][1])
+        parBorder = int(min(bbH,bbW) * 0.15)
+        # parBorder = 5
+        print(bbox)
+        print ('Border parameter: %s' % parBorder)
+        #
+        texton = cropTexton(img, bbox, brdPx=parBorder, brdPrcnt=None)
+
+        ptrnSize = parBorder+2
+        brdSizeExt = int(1.7*parBorder)
+        ptrnLef = texton[ptrnSize:-ptrnSize, :ptrnSize]
+        ptrnTop = texton[:ptrnSize, ptrnSize:-ptrnSize]
+        print(texton.shape)
+        print(ptrnLef.shape)
+        ccMapH = cv2.matchTemplate(texton, ptrnLef, method=parMethod)
+        ccMapV = cv2.matchTemplate(texton, ptrnTop, method=parMethod)
+        if parMethod==cv2.TM_SQDIFF or parMethod==cv2.TM_SQDIFF_NORMED:
+            ccMapH = 1. - ccMapH
+            ccMapV = 1. - ccMapV
+        minH = ccMapH.min()
+        minV = ccMapV.min()
+        ccMapHflt = ccMapH.copy()
+        ccMapVflt = ccMapV.copy()
+        ccMapHflt[:, :-brdSizeExt] = minH
+        ccMapVflt[:-brdSizeExt, :] = minV
+        # pMaxH = np.argmax(ccMapHflt)
+        # pMaxV = np.argmax(ccMapVflt)
+        _, maxVH, _, posMaxH = cv2.minMaxLoc(ccMapHflt)
+        _, maxVV, _, posMaxV = cv2.minMaxLoc(ccMapVflt)
+
+        #print(maxVH + maxVV)
+        if (maxCorr < maxVH + maxVV):
+            #print('#############################################################')
+            bestBBox = bbox
+            shiftH_X, shiftH_Y = posMaxH
+            shiftV_X, shiftV_Y = posMaxV
+            posRT = (posMaxH[0], posMaxH[1] - 1*ptrnSize)
+            posLB = (posMaxV[0] - 1*ptrnSize, posMaxV[1])
+            maxCorr = maxVH + maxVV
+            bestTexton = texton[:shiftV_Y, :shiftH_X]
+
+
+    #print(posRT, posLB)
+    #return (bestTexton, posRT, posLB)
+    return bestBBox
 
 ##########################################
-def getGoodGridPoints(vx,vy, isGood):
-    nr,nc = isGood.shape
+def getGoodGridPoints(vx,vy, mask):
+    nr,nc = mask.shape
     lstXX = []
     lstYY = []
     for rr in range(nr):
         for cc in range(nc):
-            if isGood[rr,cc]:
+            if mask[rr,cc]:
                 x0 = vx[rr, cc]
                 y0 = vy[rr, cc]
                 lstXX.append(x0)
                 lstYY.append(y0)
     return np.array([lstXX,lstYY]).transpose()
 
-def cropTexton(timg, texBBox, brdPrcnt=0.1, brdPx=None):
-    tsiz = np.array(timg.shape[:2])
+def cropTexton(img, texBBox, brdPrcnt=0.1, brdPx=None):
+    tsiz = np.array(img.shape[:2])
     xmin = int(texBBox[0][0])
     xmax = int(texBBox[0][1])
     ymin = int(texBBox[1][0])
@@ -292,90 +292,117 @@ def cropTexton(timg, texBBox, brdPrcnt=0.1, brdPx=None):
     else:
         dr = int(brdPrcnt * np.abs(ymax - ymin))
         dc = int(brdPrcnt * np.abs(xmax - xmin))
-    if timg.ndim<3:
-        tret = timg[ymin - dr:ymax + dr, xmin - dc:xmax + dc].copy()
+    if img.ndim<3:
+        tret = img[ymin - dr:ymax + dr, xmin - dc:xmax + dc].copy()
     else:
-        tret = timg[max(ymin - dr, 0):min(ymax + dr, timg.shape[0]), 
-                    max(xmin - dc, 0):min(xmax + dc, timg.shape[1]), :].copy()
+        tret = img[max(ymin - dr, 0):min(ymax + dr, img.shape[0]), 
+                   max(xmin - dc, 0):min(xmax + dc, img.shape[1]), :].copy()
     return tret
 
 ##########################################
 if __name__ == '__main__':
     fidx = '../../data/data04_for_test1_results_v1/txt02_pxy_M/cropped_and_results/idx.txt'
     # fidx = '/home/ar/github.com/Texture_Detection_and_Synthesis_Experiments.git/data/data04_for_test1_results_v1/txt02_pxy_M/cropped_and_results/idx.txt'
+    
     wdir = os.path.dirname(fidx)
     with open(fidx, 'r') as f:
         lstIdx = f.read().splitlines()
     numImg = len(lstIdx)
+
     if numImg<1:
         raise Exception('Cant find image Idxs in file [%s]' % fidx)
+    
     lstPathImg = [os.path.join(wdir, '%s.jpg' % ii) for ii in lstIdx]
-    paramSizTexton=2
+    
+    paramSizTexton = 2
+    texton_from_big = True
     for ii,pathImg in enumerate(lstPathImg):
         # if ii!=9:
         #     continue
         print ('[%d/%d] : %s' % (ii, numImg, pathImg))
-        tidx = lstIdx[ii]
-        timg = np.array(cv2.imread(pathImg))
-        big_img = np.array(cv2.imread(os.path.join(wdir, '%s_big.jpg' % tidx)))
 
-        [v_x, v_y, is_good] = ReadGraph(wdir, tidx)
-        # arrXY = getGoodGridPoints(v_x, v_y, is_good)
-        #FIxME: remove buttons
-        # is_good[:, is_good.shape[1] / 2] = False
-        isGoodMsk = maskErosion(is_good, percent_of_src = 0.6, min_square = 10)
-        retBBox = getBestTexton(big_img, v_x, v_y, isGoodMsk, sizN=paramSizTexton)
-        # retBBox = getRandomTexton(v_x, v_y, isGoodMsk, sizN=paramSizTexton)
-        print (retBBox)
-        bbW = np.abs(retBBox[0][0] - retBBox[0][1])
-        bbH = np.abs(retBBox[1][0] - retBBox[1][1])
-        parBorder = int(min(bbH,bbW) * 0.15)
-        # parBorder = 5
+		##### READ INPUT
+        img_idx = lstIdx[ii]
+        img = np.array(cv2.imread(pathImg))
+
+        if texton_from_big:
+        	big_img = np.array(cv2.imread(os.path.join(wdir, '%s_big.jpg' % img_idx)))
+        else:
+        	big_img = None
+
+        [v_x, v_y, mask] = readGrid(wdir, img_idx)
+
+        ##### GET TEXTON
+        #FIxME: remove buttons (better create function)
+        # mask[:, mask.shape[1] / 2] = False
+        ### get bbox
+        eroded_mask = maskErosion(mask, percent_of_src = 0.6, min_square = 10)
+        bbox = getBestTextonBBox(big_img, v_x, v_y, eroded_mask, sizN=paramSizTexton)
+        # bbox = getRandomTextonBBox(v_x, v_y, eroded_mask, sizN=paramSizTexton)
+        print (bbox)
+        ### calc border
+        bbW = np.abs(bbox[0][0] - bbox[0][1])
+        bbH = np.abs(bbox[1][0] - bbox[1][1])
+        parBorder = int(min(bbH, bbW) * 0.15) 
         print ('Border parameter: %s' % parBorder)
-        #
-        arrXY = getGoodGridPoints(v_x, v_y, isGoodMsk)
-        imgTexton = cropTexton(big_img, retBBox, brdPx=parBorder, brdPrcnt=None)
 
-        # imgTextonCorr = MinimizePatternByTemplMatchV1(imgTexton)
-        # imgTextonCorr = MinimizePatternByTemplMatchV2(imgTexton, brdSize=parBorder)
-        # imgTextonCorr, pdRightY, pdBottomX = MinimizePatternByTemplMatchV3(imgTexton, brdSize=parBorder)
-        imgTextonCorr, posRT, posLB = MinimizePatternByTemplMatchV3(imgTexton, brdSize=parBorder)
-        # genTexture = generateTiledTextonV2(imgTextonCorr, posRT)
-        genTexture = generateTiledTextonV2(imgTexton, posRT, posLB)
-        imgTextonCorrTiled   = np.tile(imgTextonCorr, (9, 9, 1))
-        # imgTextonCorrTiled = genTexture
-        imgTextonStupidTiled = np.tile(imgTexton, (9, 9, 1))
-        #
+        texton = cropTexton(big_img, bbox, brdPx=parBorder, brdPrcnt=None)
+
+        ##### PROCESS TEXTON
+        # corr_texton = MinimizePatternByTemplMatchV1(texton)
+        # corr_texton = MinimizePatternByTemplMatchV2(texton, brdSize=parBorder)
+        # corr_texton, pdRightY, pdBottomX = MinimizePatternByTemplMatchV3(texton, brdSize=parBorder)
+        corr_texton, posRT, posLB = MinimizePatternByTemplMatchV3(texton, brdSize=parBorder)
+        
+        ##### TILE TEXTON
+        nrow_tile = 3
+        ncol_tile = 3
+        stupid_tiled_texture = np.tile(texton, (nrow_tile, ncol_tile, 1))
+        corr_tiled_texture   = np.tile(corr_texton, (nrow_tile, ncol_tile, 1))
+        generated_texture = generateTiledTextureV2(texton, posRT, posLB, nrow_tile, ncol_tile)
+
+        ##### PLOT RESULTS
         plt.figure()
+
         tmpH = plt.subplot(2, 3, 1)
-        plt.hold(True)
-        plt.imshow(timg)
-        plt.plot(arrXY[:,0], arrXY[:,1], 'or')
-        small_bbox = retBBox * 480 / 1500
-        sbbW = np.abs(small_bbox[0][0] - small_bbox[0][1])
-        sbbH = np.abs(small_bbox[1][0] - small_bbox[1][1])
+        plt.title('Corr-Grid-of-Points')
+        plt.imshow(img)
+        ### plot points
+        good_grid_pts = getGoodGridPoints(v_x, v_y, eroded_mask)
+        plt.plot(good_grid_pts[:,0], good_grid_pts[:,1], 'or')
+        ### plot bboxes
+        if texton_from_big:
+	        small_bbox = bbox * img.shape[0] / big_img.shape[0]
+	        sbbW = np.abs(small_bbox[0][0] - small_bbox[0][1])
+	        sbbH = np.abs(small_bbox[1][0] - small_bbox[1][1])
+        else:
+	        small_bbox = bbox
+	        sbbW = bbW
+	        sbbH = bbH
         tp1 = patches.Rectangle((small_bbox[0][0], small_bbox[1][0]), sbbW, sbbH,     fill=False, linewidth=3, edgecolor='g')
         tp2 = patches.Rectangle((small_bbox[0][0], small_bbox[1][0]), 2*sbbW, 2*sbbH, fill=False, linewidth=2, edgecolor='g')
         tmpH.add_patch(tp1)
         tmpH.add_patch(tp2)
-        plt.hold(False)
-        plt.title('Corr-Grid-of-Points')
+
         plt.subplot(2, 3, 2)
-        plt.imshow(np.dstack((is_good, isGoodMsk, is_good)))
-        plt.title('Good-Mask')
+        plt.title('Mask')
+        plt.imshow(np.dstack((mask, eroded_mask, mask)))
+
         tmpH = plt.subplot(2, 3, 3)
-        plt.hold(True)
-        plt.imshow(imgTexton)
-        plt.title('Random Texton [%dx%d]' % (paramSizTexton,paramSizTexton))
-        plt.hold(False)
+        plt.title('Random Texton [%dx%d]' % (paramSizTexton, paramSizTexton))
+        plt.imshow(texton)
+
         plt.subplot(2, 3, 4)
-        plt.imshow(imgTextonCorrTiled)
-        plt.title('Corr-Synthesis V1')
-        plt.subplot(2, 3, 5)
-        plt.imshow(genTexture)
-        plt.title('Corr-Synthesis V2')
-        plt.subplot(2, 3, 6)
-        plt.imshow(imgTextonStupidTiled)
         plt.title('Texture Synth: periodic tiling')
+        plt.imshow(stupid_tiled_texture)
+
+        plt.subplot(2, 3, 5)
+        plt.title('Corr-Synth: horiz, vert shifts')
+        plt.imshow(corr_tiled_texture)
+
+        plt.subplot(2, 3, 6)
+        plt.title('Corr-Synth: arbitrary shifts')
+        plt.imshow(generated_texture)
+
         plt.show()
 
